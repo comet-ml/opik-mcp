dca9796a2c932dc03a89a30ac887e2a66b3e57cc
Jacques Verre <jverre@gmail.com>
2025-03-25 10:20:40 +0000

    WIP

--- src/config.ts
+++ src/config.ts
@@ -57,6 +57,7 @@ interface OpikConfig {
   mcpEnableProjectTools: boolean;
   mcpEnableTraceTools: boolean;
   mcpEnableMetricTools: boolean;
+  mcpEnablePromptOptimizationTools: boolean;
 }
 
 /**
@@ -169,6 +170,7 @@ function parseCommandLineArgs() {
       disableProjectTools?: boolean;
       disableTraceTools?: boolean;
       disableMetricTools?: boolean;
+      mcpEnablePromptOptimizationTools?: boolean;
       [key: string]: unknown;
     }
   );
@@ -177,7 +179,7 @@ function parseCommandLineArgs() {
 /**
  * Load environment variables with fallbacks
  */
-function loadConfig(): OpikConfig {
+export function loadConfig(): OpikConfig {
   // Parse command-line arguments first
   const args = parseCommandLineArgs();
 
@@ -224,6 +226,9 @@ function loadConfig(): OpikConfig {
     mcpEnableMetricTools: args.disableMetricTools
       ? false
       : process.env.MCP_ENABLE_METRIC_TOOLS !== 'false', // Enable by default
+    mcpEnablePromptOptimizationTools: args.disablePromptOptimizationTools
+      ? false
+      : process.env.MCP_ENABLE_PROMPT_OPTIMIZATION_TOOLS !== 'false', // Enable by default
   };
 
   // Validate required fields but be much more forgiving
